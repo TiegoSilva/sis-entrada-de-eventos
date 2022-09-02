cce5381fffca41ee8f2fdbb479702e5498f4a4fd
TiegoSilva <tiego.silvaa@gmail.com>
2022-09-02 14:10:37 -0300

    corrigindo login do envio de e-mail

--- index.js
+++ index.js
@@ -446,12 +446,12 @@ let storage = multer.diskStorage(
 
   
   // create reusable transporter object using the default SMTP transport
-  let transporter = nodemailer.createTransport({
+   let transporter = nodemailer.createTransport({
     host: "mail.gestaogma.com.br",
     port: 465,
     secure: true, // true for 465, false for other ports
     auth: {
-      user: 'no-reply@gestaogma.com.br	', 
+      user: 'no-reply@gestaogma.com.br', 
       pass: '@Gma123456', 
     },
     tls: {
